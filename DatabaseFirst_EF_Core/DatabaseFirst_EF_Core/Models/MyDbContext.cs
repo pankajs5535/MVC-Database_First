4f8eaa8a7014c9c28000c8ae0afeb3210dc6359f
pankajs5535 <pankajnsuryawanshi@gmail.com>
2024-11-02 11:42:16 +0530

    Database First Code

---
+++ DatabaseFirst_EF_Core/DatabaseFirst_EF_Core/Models/MyDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirst_EF_Core.Models;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=SKYLARK; Database=Object_Tech; Trusted_Connection=True;Encrypt=False; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("Stud_Name");
+            entity.Property(e => e.Surname)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("Stud_Surname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DatabaseFirst_EF_Core/DatabaseFirst_EF_Core/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseFirst_EF_Core.Models;
+
+public partial class Student
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column("Stud_Name", TypeName = "varchar(50)")]
+    public string? Name { get; set; }
+
+    [Column("Stud_Surname", TypeName = "varchar(50)")]
+    public string? Surname { get; set; }
+
+    public Gender Gender { get; set; }
+
+    public int? Age { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? City { get; set; }
+
+
+}
+
+public enum Gender
+{
+    Male, Female
+}
+
